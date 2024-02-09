89d45278a7b27f98e82e261f694864d211f44090
OnyxRader <roundythomas@gmail.com>
2024-02-08 20:10:34 -0700

    Initial commit

---
+++ src/main/java/net/onyx/attemptmod/ExampleMod.java
@@ -0,0 +1,16 @@
+package net.onyx.attemptmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExampleMod implements ModInitializer {
+	public static final String MOD_ID = "ExampleMod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		System.out.println("Onyx's First Mod loaded");
+	}
+}
---
+++ src/main/java/net/onyx/attemptmod/ExampleModClient.java
@@ -0,0 +1,10 @@
+package net.onyx.attemptmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExampleModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
